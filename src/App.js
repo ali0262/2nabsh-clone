448c69ce89a5bffa43b063ffa5dee2dee74e3451
ali <aliwebmaster021@gmail.com>
2023-02-27 20:27:55 +0330

    2nabsh-clone with react (created pages & routes)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
+import Home from './Pages/Home';
+import BuyHouse from './Pages/BuyHouse';
+import RentHouse from './Pages/RentHouse';
+import Profile from './Pages/Profile';
+import RealStates from './Pages/RealStates';
+import Adversting from './Pages/Adversting';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/buy-house' element={<BuyHouse/>}/>
+      <Route path='/rent-house' element={<RentHouse/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/ad' element={<Adversting/>}/>
+      <Route path='/real-states' element={<RealStates/>}/>
+    </Routes>
+    </>
   );
 }
 
---
+++ src/Pages/Adversting.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Adversting() {
+  return (
+    <div>Adversting</div>
+  )
+}
